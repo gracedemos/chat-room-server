cb55562b1c4686e2ea8923b17854bacf998a3917
gracedemos <graysondemos@gmail.com>
2022-11-12 02:00:44 -0500

    Fixed cors

--- src/main/java/com/gracedemos/chat_room_server/Controller.java
+++ src/main/java/com/gracedemos/chat_room_server/Controller.java
@@ -1,5 +1,6 @@
 package com.gracedemos.chat_room_server;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,11 +14,13 @@ public class Controller {
         chatRoom = new ChatRoom();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ChatRoom getChatRoom() {
         return chatRoom;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public String addMessage(@RequestBody String[] message) {
         chatRoom.addMessage(message[0], message[1]);
